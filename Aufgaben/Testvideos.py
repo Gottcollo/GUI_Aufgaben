e27e158cdd2e9f9d6f29d5bb3d50711f863f2f4d
Gottcollo <gottcollo@gmail.com>
2025-06-02 12:07:11 +0200

    hab einfach ein button gemacht xD

--- Aufgaben/Testvideos.py
+++ Aufgaben/Testvideos.py
@@ -1,3 +1,10 @@
 import tkinter as tk
 
-tk._test()
+root = tk.Tk()
+
+label1 = tk.Label(root, text='Hallo Welt')
+button1 = tk.Button(root, text='Klick')
+label1.pack()
+button1.pack()
+root.mainloop()
+
