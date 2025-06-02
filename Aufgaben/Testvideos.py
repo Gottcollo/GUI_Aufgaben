124d9940e851a6f3c3a50824b0942bb21515d435
Gottcollo <gottcollo@gmail.com>
2025-06-02 12:12:40 +0200

    kommentiert#

--- Aufgaben/Testvideos.py
+++ Aufgaben/Testvideos.py
@@ -2,9 +2,11 @@ import tkinter as tk
 
 root = tk.Tk()
 
-label1 = tk.Label(root, text='Hallo Welt')
-button1 = tk.Button(root, text='Klick')
-label1.pack()
+label1 = tk.Label(root, text='Hallo Welt') #immer auf das Fatherfenster setzen
+button1 = tk.Button(root, text='Klick') #habe einfach einen button hinzugefügt 
+label1.pack() #muss "verpackt" werden damit es genutzt wird 
 button1.pack()
 root.mainloop()
+
+print('Air')
 
